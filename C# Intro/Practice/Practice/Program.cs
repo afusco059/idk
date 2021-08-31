4cee3e512a1e568972d6dd0c5c4e4b6feb91cad7
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-08-31 08:39:58 -0400

    Add files via upload

---
+++ C# Intro/Practice/Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello what is your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Welcome " + userName);
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
+
+            Console.Write("how many hours of sleep did you get last night");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if (hoursOfSleep >= 8)
+            {
+                Console.WriteLine("good job thats a lot of sleep");
+            }
+            else if (hoursOfSleep >= 6) 
+            {
+                Console.WriteLine("Not bad but you need more sleep");
+            
+            }
+            else if (hoursOfSleep < 6)
+            {
+                Console.WriteLine("you need more sleep");
+
+            
+            }
+            else
+            {
+                Console.WriteLine("None it terrible");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
